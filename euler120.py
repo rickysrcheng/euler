643fb7e8cb7987f9bb1812f5c975ddc78aae0d2f
scheng0209 <rsrcheng9@gmail.com>
2016-04-16 01:56:03 -0500

    Square Remainders

---
+++ euler120.py
@@ -0,0 +1,45 @@
+__author__ = 'TwoZer0Nine'
+# result 333082500 returned in 0.000453948974609 seconds
+
+import time
+# import matplotlib.pyplot as plt
+#
+# x = []
+# y = []
+#
+# for num in range(3, 21):
+#     n = num**2
+#     maximum = 0
+#     if num < 21:
+#         compare = 0.5
+#     elif num < 50:
+#         compare = 0.9
+#     elif num < 120:
+#         compare = 0.96
+#     else:
+#         compare = 0.98
+#     while n > float(num**2)*compare:
+#         c = ((num - 1)**n + (num + 1)**n) % (num**2)
+#         if c > maximum:
+#             maximum = c
+#         n -= 1
+#     x.append(num)
+#     y.append(float(maximum)/float(num**2))
+#
+# plt.plot(x, y)
+# plt.xlabel('Number')
+# plt.ylabel('Ratio')
+# plt.grid(True)
+# plt.show()
+#
+
+start = time.time()
+
+result = 0
+for i in xrange(3, 1001):
+    if i % 2 == 0:
+        result += i*(i-2)
+    else:
+        result += i * (i-1)
+
+print "result %s returned in %s seconds" % (result, time.time()-start)
